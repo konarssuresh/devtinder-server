532ad74d4936caef5548eebe5fd336c798fd8b52
Suresh Konar <konarssuresh@gmail.com>
2025-10-19 17:57:33 +0530

    crud api implementation in db

--- src/app.js
+++ src/app.js
@@ -16,6 +16,84 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const resp = await User.findOne({
+      emailId: userEmail,
+    });
+
+    if (resp === null) {
+      res.status(404).send("User not Found");
+    } else {
+      res.send(resp);
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("error fetching user by email");
+  }
+});
+
+// feed api- get all users from database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send("something went wrong");
+  }
+});
+
+app.get("/userbyid", async (req, res) => {
+  const id = req.body.id;
+  try {
+    let user = await User.findById(id);
+    if (user === null) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const id = req.body.id;
+  try {
+    const dbResp = await User.findByIdAndDelete(id);
+    if (dbResp === null) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(dbResp);
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  const id = data.id;
+
+  try {
+    const user = await User.findByIdAndUpdate(id, data, {
+      returnDocument: "after",
+    });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("something went wrong");
+  }
+});
+
 connectDb()
   .then(() => {
     console.log("DB connection established");
